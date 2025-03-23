de042aabf72c1c1b99f84bd0f91cd9166bd53ec5
Bala2418 <bobbus4@gmail.com>
2025-03-23 19:07:37 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		 
+		float versionValueOf=91.0f;
+		String developerValueOf="google";
+		boolean betaValueOf=false;
+		int yearOfValue=2008;
+		char shortKeyValueOf='c';
+		
+		System.out.println(developerValueOf+" "+versionValueOf+" "+yearOfValue+" "+shortKeyValueOf+" "+betaValueOf);
+		
+		
+		
+
+	}
+
+}
---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,35 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		 //Age is 35
+		
+		//int experience=12;
+
+		byte age=12;
+		System.out.println(age);	
+		
+		short number=23234;
+		System.out.println(number);
+		
+		int numberOfDivi=4546677;
+		System.out.println(numberOfDivi);
+		
+		long digitOfLong=6788765l;
+		System.out.println(digitOfLong);
+		
+		float decimalsIsTwo=956.6756f;
+		System.out.println(decimalsIsTwo);
+		
+		char isSingle='o';
+		 
+		boolean haveGot46=true;
+		
+		String name="Bala";
+		System.out.println(name+" "+isSingle);
+				
+				
+	}
+
+}
